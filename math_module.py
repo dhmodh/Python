9f097f680a7a4719f5ee7a7fd661e9655da8d2b7
dhmodh <dishantdmx2000@gmail.com>
2020-04-13 19:43:22 +0530

    Add files via upload
    Importing Module's in Python

---
+++ math_module.py
@@ -0,0 +1,18 @@
+import math
+
+x = 2.25
+y = 3
+
+#square root of x
+result = math.sqrt(x)
+print(result)
+
+#x is raised to the power of y
+result = math.pow(x, y)
+print(result)
+
+#value of pi
+print(math.pi)
+
+#value of e
+print(math.e)
