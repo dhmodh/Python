eeb683132ba00f1fdaf77b865ab4bd5ffbd8bcd5
dhmodh <dishantdmx2000@gmail.com>
2020-04-13 19:45:17 +0530

    Class And Object Basic Program

---
+++ class_1.py
@@ -0,0 +1,14 @@
+class Number:
+	
+	def add(self, a, b):
+		return a + b
+		
+	def multiply(self, a, b):
+		return a * b
+		
+n1 = Number()
+sum = n1.add(2,3)
+print(sum)
+
+product = n1.multiply(2,5)
+print(product)
